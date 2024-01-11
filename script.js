9e6982c92b007ae85eada819c678412e1ab49450
JotaJotaM1 <johnjames_89@hotmail.com>
2024-01-11 14:36:20 -0500

    registration page restructuring, new form

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/* Responsive header with navigation menu */
+
 const nav = document.querySelector("#nav");
 const open = document.querySelector("#open");
 const close = document.querySelector("#close");
@@ -10,5 +12,40 @@ close.addEventListener("click", () => {
   nav.classList.remove("visible");
 });
 
+/* active or desactive placeholder*/
 
+function clearPlaceholder(element) {
+  element.setAttribute("placeholder", "");
+}
 
+function restorePlaceholder(element) {
+  if (element.value === "") {
+      element.setAttribute("placeholder", element.getAttribute("data-original-placeholder"));
+  }
+}
+
+/* Validate password */
+
+const passwordInput = document.getElementById("password");
+const confirmPasswordInput = document.getElementById("confirmPassword");
+const confirmError = document.getElementById("confirmError");
+
+function validatePassword() {
+  const password = passwordInput.value;
+  const confirmPassword = confirmPasswordInput.value;
+
+  if (password === "" || confirmPassword === "") {
+    confirmError.textContent = "";
+  } else if (confirmPassword !== password) {
+    confirmError.textContent = "Passwords do not match.";
+    confirmError.style.color = "red";
+  } else {
+    confirmError.textContent = "Passwords match.";
+    confirmError.style.color = "green";
+  }
+}
+
+function validateForm() {
+  validatePassword();
+  return confirmError.style.color === "green";
+}
