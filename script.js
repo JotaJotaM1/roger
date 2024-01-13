63336d204281eddc1aff5032bb508e0bcf5959e6
JotaJotaM1 <johnjames_89@hotmail.com>
2024-01-13 16:46:21 -0500

    Carousel for our customers finished

--- script.js
+++ script.js
@@ -37,3 +37,56 @@ function validateForm() {
   validatePassword();
   return confirmError.style.color === "green";
 }
+
+
+/* Validate password */
+
+document.addEventListener("DOMContentLoaded", function () {
+  let currentSlide = 0;
+  const slides = document.querySelectorAll(".carousel-slide");
+  const prevBtn = document.getElementById("prevBtn");
+  const nextBtn = document.getElementById("nextBtn");
+
+  // Función para mostrar una imagen específica
+  function showSlide(index) {
+    slides.forEach((slide, i) => {
+      if (i === index) {
+        slide.style.display = "block";
+      } else {
+        slide.style.display = "none";
+      }
+    });
+  }
+
+  // Muestra la primera imagen al cargar la página
+  showSlide(currentSlide);
+
+  // Función para mostrar la siguiente imagen
+  function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+  }
+
+  // Función para mostrar la imagen anterior
+  function prevSlide() {
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+  }
+
+  // Agrega eventos a los botones de navegación
+  if (nextBtn) {
+    nextBtn.addEventListener("click", function() {
+      nextSlide();
+    });
+  }
+
+  if (prevBtn) {
+    prevBtn.addEventListener("click", function() {
+      prevSlide();
+    });
+  }
+});
+
+
+
+
